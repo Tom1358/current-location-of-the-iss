d0b67775db839b1791ca3240ba846f101c8ef3e9
Tom1358 <thomasellis358@hotmail.com>
2021-05-04 20:15:18 +0000

    Create map with ISS location on iss.html

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
-/*var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoidG9tMTM1OCIsImEiOiJja284aDJydGQybWc1MnhwZ2V0aWUxb2JpIn0.0hLrp4EP06L3YXBgr7-B1A'
-}).addTo(mymap);
-
-console.log('about to fetch a rainbow');
-fetch('/rainbow.jpeg').then(response => {
-    console.log(response);
-    return response.blob().then(blob => {
-        console.log(blob);
-        document.getElementById('rainbow').src = URL.createObjectURL(blob);
-    })
-    .catch(error => {
-        console.log('error!');
-        console.error(error);
-    })
-    });
 
 // data from https://data.giss.nasa.gov/gistemp/
 
@@ -43,7 +21,7 @@ async function getData() {
     return {xs, ys};
 }
 
-chartIt();
+/*chartIt();
 async function chartIt() {
     const data = await getData();
     const ctx = document.getElementById('chart').getContext('2d');
@@ -73,19 +51,6 @@ async function chartIt() {
             }
         }
     });
-}*/
+}
+*/
 
-
-
-const api_url="https://api.wheretheiss.at/v1/satellites/25544";
-async function getISS() {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    const { latitude, longitude } = data;
-    document.getElementById('lat').textContent = latitude;
-    document.getElementById('lon').textContent = longitude;
-    console.log(latitude);
-    console.log(longitude);
-};
-
-getISS();
